a3ee6076cd335c5479746511ca33af12ffd8b482
hadeelfleifel <51743597+hadeelfleifel@users.noreply.github.com>
2019-07-21 10:18:05 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,172 @@
+console.log('GOOD LUCK 👩‍💻 👨‍💻')
+/* Q1:
+Using For Each
+Create a function called addKeyPowerIndex
+that takes an array of objects as a parameter
+and return this array with add a key to each object
+that is the (index) of this object in this array
+and the value will be the (power) of the index
+
+var arrOfObj1 = [
+  { a: 12 },
+  { b: 5 },
+  { c: 16 },
+  { d: 4 },
+  { e: 3 }
+]
+
+Ex: addKeyPowerIndex(arrOfObj1)
+=> [
+  {0: 0, a: 12},
+  {1: 1, b: 5},
+  {2: 4, c: 16},
+  {3: 9, d: 4},
+  {4: 16, e: 3}
+] 
+
+var arrOfObj2 = [
+  { a: 'cat' },
+  { b: 'dog' },
+  { c: 'duck' }
+]
+
+Ex: addKeyPowerIndex(arrOfObj2)
+=> [
+  {0: 0, a: 'cat'},
+  {1: 1, b: 'dog'},
+  {2: 4, c: 'duck'}
+]   
+*/
+var arrOfObj1 = [
+  { a: 12 },
+  { b: 5 },
+  { c: 16 },
+  { d: 4 },
+  { e: 3 }
+]
+
+function addKeyPowerIndex(arr) {
+	var newArr = arr.forEach (function(a,b)
+		{
+			a[b]=a**2 //if a**2 edit in values without add new key ; b => name of the key ;a[b] => method to add new key; if add return  {0: NaN, a: 12}  
+		}
+	);
+	return arr
+}
+addKeyPowerIndex(arrOfObj1)
+
+/* Q2:
+Usind Map
+Create a function called decreseBy
+that takes an array of numbers and number as a parameter
+and return a new array after will decrease this number 
+from each element in this array
+
+var arrOfNum1 = [77,5,33]
+Ex: decreseBy(arrOfnum1,10)
+=> [67,-5,23]
+
+Ex: decreseBy(arrOfNum1,-6)
+=> [83, 11, 39]
+*/
+var arrOfNum1 = [77,5,33]
+function decreseBy(arr,num) {
+	var newArr=arr.map(a=>  a-num); // not reduce coz the result not one value , not filter coz no condition.
+	return newArr;
+}
+decreseBy(arrOfNum1,10)
+
+/* Q3:
+Using Filter
+Create a function called nameLongerThan
+that takes an array of objects and number as a parameter
+and return a new array with only the object has a value inside 
+the key name longer than this number
+
+var arrOfObj4 = [
+  { name: "alex" },
+  { name: "mercer"},
+  { name: "alice" },
+  { name: "zaheer"},
+  { name: "elizabeth"}
+]
+Ex: nameLongerThan(arrOfObj4,4)
+=>[
+  { name: "mercer"},
+  { name: "alice" },
+  { name: "zaheer"},
+  { name: "elizabeth"}
+]
+
+Ex: nameLongerThan(arrOfObj4,6)
+=>[
+  { name: "elizabeth"}
+]
+*/
+var arrOfObj4 = [
+  { name: "alex" },
+  { name: "mercer"},
+  { name: "alice" },
+  { name: "zaheer"},
+  { name: "elizabeth"}
+]
+function nameLongerThan(arr,num) {
+	var newArr = arr.filter(a =>
+    	(a.name.length > num) // num = 4 ;a.length =5 ; newArr =4 must;
+
+
+		); return newArr
+}
+nameLongerThan(arrOfObj4,4)
+
+
+/* Q4:
+Using Reduce
+Create a function called avgLength
+that takes an array of objects of strings and key as a parameter
+and return the avg of the length of this key inside this objects
+
+var arrOfObj4 = [
+  { name: "alex", food: "fried chiken" },
+  { name: "mercer", food: "pizaa" },
+  { name: "alice", food: "burger" },
+  { name: "zaheer", food: "hot dog" },
+  { name: "elizabeth", food: "eggs" }
+]
+Ex: avgLength(arrOfObj4,"name")
+=> 6
+
+Ex: avgLength(arrOfObj4,"food")
+=> 6.8
+*/
+var arrOfObj4 = [
+  { name: "alex", food: "fried chiken" },
+  { name: "mercer", food: "pizaa" },
+  { name: "alice", food: "burger" },
+  { name: "zaheer", food: "hot dog" },
+  { name: "elizabeth", food: "eggs" }
+]
+
+function avgLength(arr,key) {
+
+	var newArr=arr.reduce((a,b)=> {
+	 return a + b[key].length // a=0 ; fKey = 4 ,sKey=6,... total = 30;
+	}, 0);	 
+	 return newArr/ arr.length // 30 /5 /6
+
+}
+ avgLength(arrOfObj4,"name");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
